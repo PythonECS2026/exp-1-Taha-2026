dd1f4bd156328910ebb0fe23cd2d0e68b6154c2f
Taha-2026 <idrisi.abu.tahaa@gmail.com>
2026-03-03 17:27:23 +0530

    Implement personalized greeting and age display
    Added user input for name and age, and personalized greeting.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # AIM: Create a personalized greeting message
-#      User enters name: eg: Shibu. and the program should print "Hello, Shibu" 
-# Coder: 
-# Date:
+#Coder: ABUTAHA IDRISI
+#Date: 3/3/26
 
-# Write your code here
+name=input("Enter Name: ")
+age=input("Enter Age: ")
+print("Welcome to Rizvi College of Engineering")
+print("Hello,", name)
+print("You are", age, "years old.")
